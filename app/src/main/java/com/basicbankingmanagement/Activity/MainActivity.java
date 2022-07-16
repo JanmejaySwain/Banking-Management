4a3c905b4fc292c3e54f59fcf1769953537ee06d
janu <janmejaswain2001@gmail.com>
2022-07-16 13:09:34 +0530

    initial commit

--- app/src/main/java/com/basicbankingmanagement/Activity/MainActivity.java
+++ app/src/main/java/com/basicbankingmanagement/Activity/MainActivity.java
@@ -38,19 +38,19 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        dbhelper.addContacts("Malaya ranjan Dash", "1111101", "10000", "5454545454654");
-        dbhelper.addContacts("Sunil Kumar Sahu", "1111103", "10000", "46546546546546");
-        dbhelper.addContacts("Jashobanta kumar sa", "1111111", "10000", "65465465465465");
-        dbhelper.addContacts("Suroj Subudhi", "11111012", "10000", "46546546546546");
-        dbhelper.addContacts("Satya siba Sahu", "1111113", "10000", "65465465465465");
+        dbhelper.addContacts("Gayatri Mohapatra", "1111103", "10000", "5454545454654");
+        dbhelper.addContacts("Jagamohan Swain", "1111102", "10000", "46546546546546");
+        dbhelper.addContacts("Samarendra Swain", "1111111", "10000", "65465465465465");
+        dbhelper.addContacts("Akuli Swain", "11111012", "10000", "46546546546546");
+        dbhelper.addContacts("Subhalaxmi Swain", "1111113", "10000", "65465465465465");
         dbhelper.addContacts("Kriti Kumar Behera", "1111109", "10000", "9090323291");
-        dbhelper.addContacts("Janmejay Swain", "1111102", "10000", "65465456");
-        dbhelper.addContacts("Mrityunjay Patra", "1111103", "10000", "654654654654");
-        dbhelper.addContacts("Soumya Ranjan Das", "1111104", "10000", "5454456454");
-        dbhelper.addContacts("Papoo sahu", "1111105", "10000", "54544654654654");
-        dbhelper.addContacts("Biswasmurti Sahu", "1111106", "10000", "5445454465");
-        dbhelper.addContacts("Manas ranjan Mohanta", "1111107", "10000", "554545454545");
-        dbhelper.addContacts("Sunil Kumar Sahu", "1111108", "10000", "45454544545454");
+        dbhelper.addContacts("Janmejay Swain", "1111101", "10000", "65465456");
+        dbhelper.addContacts("Mrityunjay Patra", "1111110", "10000", "654654654654");
+        dbhelper.addContacts("Saurav Sharma", "1111107", "10000", "5454456454");
+        dbhelper.addContacts("Chitaranjan Sethy", "1111105", "10000", "54544654654654");
+        dbhelper.addContacts("Pratush Kumar Tripathy", "1111106", "10000", "5445454465");
+        dbhelper.addContacts("Rajashree Mohapatra", "1111104", "10000", "554545454545");
+        dbhelper.addContacts("Sritam Samal", "1111108", "10000", "45454544545454");
         Log.d("MainActivity", "DATABASE END INSERT");
         viewUser.setOnClickListener(new View.OnClickListener() {
             @Override
